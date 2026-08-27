1e81a04355c53de775e58f6c2ecc8d248497baa9
greydev <obebenny28@gmail.com>
2026-08-27 20:13:13 +0100

    file modigication

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -8,7 +8,7 @@ import {
   CircleDollarSign,
   Coins,
   ExternalLink,
-  File,
+  FileCheck,
   Flame,
   Gamepad2,
   Github,
@@ -63,7 +63,7 @@ const icons = {
   github: Github, hand: HandCoins, lock: LockKeyhole, menu: Menu, telegram: TelegramLogo,
   rocket: Rocket, shield: ShieldCheck, smartphone: Smartphone, sparkles: Sparkles, shopping: ShoppingCart,
   wallet: WalletCards, x: XLogo, zap: Zap, real_x: XIcon, network: Network,
-  leafpay: LeafTradeIcon,filecode: File,
+  leafpay: LeafTradeIcon,filecode: FileCheck,
 
 }
 
